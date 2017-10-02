461806b446e198ce98db9a91c572feea0695b7fe
Ravi Hasija <ravi.hasija@charter.com>
2017-10-01 20:51:19 -0700

    Improved data class example

--- src/main/kotlin/arete/example/kotlin/O1clazz/04_DataClass.kt
+++ src/main/kotlin/arete/example/kotlin/O1clazz/04_DataClass.kt
@@ -3,6 +3,24 @@ package arete.example.kotlin.O1clazz
 // Kotlin provides toString, equals and hashCode for a data class.
 data class Movie(val name: String, val year: Int) {
     override fun toString(): String {
-        return "$name was released in $year"
+        return "Movie $name was released in $year"
     }
+}
+
+fun main(args: Array<String>) {
+    val movie = Movie("Rocky", 1980)
+
+    // data class provides us:
+    //  getters and setters,
+    //  hashCode,
+    //  equals
+    //  copy
+
+    println("${movie.name} ==> ${movie.year}")
+    println(movie.toString())
+    println(movie.equals(Movie("River", 1980)))
+    println (movie.hashCode())
+    val movie2 = movie.copy(year = 1999)
+    println (movie2)
+    println(movie == movie2)
 }
